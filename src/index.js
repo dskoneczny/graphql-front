ce0a6fad09a4d4a8e3e5fc2b36352041bea3f628
Damian Skoneczny <damian.skoneczny@htdevelopers.com>
2019-03-02 14:19:02 +0100

    Implement ApolloProvider component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// https://github.com/dskoneczny/graphql-front
 // yarn add apollo-client-preset
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset' 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const httpLink = new HttpLink({ uri: 'http://localhost:4000'})
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: InMemoryCache
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
